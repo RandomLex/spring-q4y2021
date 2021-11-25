b2df84689b882891c5cb8d22ce6d6d73506f6c38
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-11-25 21:15:01 +0300

    13-spring-xml-autowiring-byType-simpe-qualifiers

---
+++ src/main/java/org/barzykin/ita/spring/model/Mentor.java
@@ -0,0 +1,26 @@
+package org.barzykin.ita.spring.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Data
+@NoArgsConstructor
+public class Mentor extends Teacher {
+    private int salary;
+    private List<String> certificates;
+
+    public Mentor(int id, String name, int salary) {
+        super(id, name);
+        this.salary = salary;
+    }
+
+    public Mentor(int id, String name) {
+        super(id, name);
+    }
+}
